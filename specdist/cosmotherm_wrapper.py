correct high-z formula

specdist/cosmotherm_wrapper.py
@@ -40,6 +40,7 @@ class cosmotherm:
 
         self.path_to_ct_param_file = path_to_cosmotherm + '/runfiles/'
         self.tmp_dir_name = 'tmp'
+        self.pi_use_zstart_from_total_energy_fraction ='no'
 
 
 
@@ -56,6 +57,47 @@ class cosmotherm:
     def compute_specdist(self,index_pval=0,**params_values_dict):
 
         p_dict = params_values_dict
+
+        # Photon Injection Specific:
+        # here check whether we require a starting redshift
+        # that depends on Gamma_inj in such way that the evolution
+        # is started when the injected energy is just a fraction of the full injected energy.
+        # In this  case, we first compute the total energetics
+        # then we look for the starting redshift by interpolation and root-finding
+
+        if (self.pi_use_zstart_from_total_energy_fraction ==  'yes'):
+            # only perform global energetics
+            p_dict['only solve global energetics'] = 1
+            # write parameter file
+            subprocess.call(['mkdir',self.path_to_ct_tmp_dir+'/tmp_'+str(index_pval)])
+            p_dict['path for output'] = self.path_to_ct_tmp_dir+'/tmp_'+str(index_pval) + '/'
+            with open(self.path_to_ct_tmp_dir+'/tmp_'+str(index_pval)+'/tmp.ini', 'w') as f:
+                for k, v in p_dict.items():
+                    f.write(str(k) + ' = '+ str(v) + '\n')
+            f.close()
+            # run cosmotherm
+            subprocess.call([path_to_cosmotherm+'/CosmoTherm',self.path_to_ct_tmp_dir+'/tmp_'+str(index_pval)+'/tmp.ini'])
+            R = np.loadtxt(p_dict['path for output']+'energetics.cooling'+self.root_name+'PDE_ODE.tmp.dat')
+            redshifts = R[:,0]
+            if (self.ct_pi_stim == 0):
+                relative_energy_integral = -R[:,16]/R[:,18][-1]
+            else:
+                relative_energy_integral = -R[:,17]/R[:,18][-1]
+            Int_rho = []
+            for zp in redshifts:
+                Int_rho.append(np.trapz(-relative_energy_integral[redshifts>zp],np.log(1.+redshifts[redshifts>zp])))
+            Int_rho = np.asarray(Int_rho)
+            f = interp1d(np.log(1.+redshifts),Int_rho)
+            def f_frac(ln1pz):
+                return f(ln1pz)-0.01
+            zinj_frac = np.exp(optimize.brentq(f_frac, np.log(1.+max(redshifts)), np.log(1.+min(redshifts))))-1.
+            p_dict['zstart'] = max(self.ct_zlate,zinj_frac)
+            # re-inititialise the parameters:
+            p_dict['only solve global energetics'] = 0
+            # remove the directory and move on:
+            subprocess.call(['rm','-rf',self.path_to_ct_tmp_dir+'/tmp_'+str(index_pval)])
+
+
         subprocess.call(['mkdir',self.path_to_ct_tmp_dir+'/tmp_'+str(index_pval)])
         p_dict['path for output'] = self.path_to_ct_tmp_dir+'/tmp_'+str(index_pval) + '/'
         with open(self.path_to_ct_tmp_dir+'/tmp_'+str(index_pval)+'/tmp.ini', 'w') as f:
@@ -92,9 +134,9 @@ class cosmotherm:
                 r_dict["t_cosmic"] = R[:,13]
                 r_dict["t_stim"] = R[:,15]
                 if (self.ct_pi_stim == 0):
-                    r_dict["[(dlnRho/dln1pz)/(dRho/Rho)_inj]"] = R[:,16]/R[:,18][-1]
+                    r_dict["[(dlnRho/dln1pz)/(dRho/Rho)_inj]"] = -R[:,16]/R[:,18][-1]
                 else:
-                    r_dict["[(dlnRho/dln1pz)/(dRho/Rho)_inj]"] = R[:,17]/R[:,18][-1]
+                    r_dict["[(dlnRho/dln1pz)/(dRho/Rho)_inj]"] = -R[:,17]/R[:,18][-1]
                 Drho_rho = p_dict['photon injection Drho_rho_dec']
                 r_dict["finj"] = Drho_rho/R[:,18][-1]
 

specdist/pispec_functions.py
@@ -2,6 +2,7 @@ from .utils import *
 from .cosmology import *
 from .cosmotherm_wrapper import *
 from .specdist_functions import *
+from .standard_mu_and_y_distortions import *
 
 
 #Particle mass in eV, m/1eV = 4.698*10^-4 x_inj
@@ -34,8 +35,23 @@ def injection_redshift_zX(gamma_inj,cosmo,cosmotherm):
     def f(lnz):
         z = np.exp(lnz)
         return gamma_inj*cosmo.t_H0_in_s()-cosmo.E(z)-cosmo.dE_dz(z)*(1.+z)
-    root = np.exp(optimize.brentq(f, np.log(cosmotherm.ct_zstart), np.log(cosmotherm.ct_zend)))
+    root = np.exp(optimize.brentq(f, np.log(1e14), np.log(cosmotherm.ct_zend)))
     return root
+
+
+# def injection_redshift_zX_stim(gamma_inj,xinj0,cosmo,cosmotherm):
+#     # set_cosmo_to_CT_cosmo_params(cosmo,cosmotherm)
+#
+#     def f(lnz):
+#         z = np.exp(lnz)
+#         xinj = xinj0/(1.+z)
+#         ninj = n_bb(xinj)
+#         # current expression not correct
+#         return gamma_inj*(1.+2.*ninj)*cosmo.t_H0_in_s()-cosmo.E(z)-cosmo.dE_dz(z)*(1.+z)
+#     root = np.exp(optimize.brentq(f, np.log(1e14), np.log(cosmotherm.ct_zend)))
+#     return root
+#
+
 
 def find_Gamma_inj_for_injection_redshift_zX(zX,cosmo,cosmotherm):
     def f(lng):
@@ -56,10 +72,11 @@ def high_redshift_f_dm_limit(mu_lim,cosmo,cosmotherm,dm_particle,*args,**kwargs)
     # return mu_lim
 
     dm_particle.f_dm = 1.
-    mui = mu_continuous_injection(cosmo,cosmotherm,dm_particle,N_int = N_int)['value']
+    mui = mu_continuous_injection(cosmo,cosmotherm,dm_particle,**kwargs)['value']
     #return mui-mu_lim
     #root = np.exp(optimize.brentq(f, np.log(1e-100), np.log(1e100)))
     return mu_lim/mui
+
 
 
 
@@ -107,12 +124,21 @@ def mu_continuous_injection(cosmo,cosmotherm,dm_particle,*args,**kwargs):
 
     #trapezoidal rule
     nz = int(N_int)
-    zend_gamma = cosmo.z_of_t(10.*1./dm_particle.Gamma_inj)
+    stim = kwargs.get('stim', 'no')
+    if stim == 'no':
+        zend_gamma = cosmo.z_of_t(10.*1./dm_particle.Gamma_inj)
+    if stim ==  'yes':
+        xinj0 = dm_particle.x_0
+        zend_gamma = z_of_t_stim(10.*1./dm_particle.Gamma_inj,xinj0,cosmo)
     zend = max(cosmo.z_end,zend_gamma)
     ln1pz_array = np.linspace((np.log(1.+cosmo.z_start)),(np.log(1.+zend)),nz)
+    #ln1pz_array = np.linspace((np.log(1.+1e14)),(np.log(1.+zend)),nz)
     Ip = []
     int_array_xp = []
-    a_args = (cosmo,dict)
+    #a_args = (cosmo,dict)
+    kwargs['cosmotherm']=cosmotherm
+    kwargs['dm_particle']=dm_particle
+    a_args = (cosmo,kwargs)
     for p in ln1pz_array:
         int_p = integrand(p,*a_args)
         int_array_xp.append(int_p)
@@ -208,8 +234,48 @@ def pi_entropy_production_history_dlnN_dt(z,cosmo,**kwargs):
     ct = kwargs['cosmotherm']
     X_dm = kwargs['dm_particle']
     delta_t = cosmo.t_of_z_in_s(z)['value'] - cosmo.t_of_z_in_s(cosmo.z_start)['value']
-    #note: this is *dependent* of x_inj
-    return f_inj(X_dm,cosmo)*X_dm.Gamma_inj*np.exp(-X_dm.Gamma_inj*delta_t)
+    stim = kwargs.get('stim', 'no')
+    if stim == 'no':
+        #note: this is *dependent* of x_inj
+        return f_inj(X_dm,cosmo)*X_dm.Gamma_inj*np.exp(-X_dm.Gamma_inj*delta_t)
+    if stim == 'yes':
+        xinj = x_inj(X_dm,z)
+        ninj = n_bb(xinj)
+        xinj0 = xinj*(1.+z)
+        delta_t_stim = t_stim_of_z_in_s(z,xinj0,cosmo)['value'] -  t_stim_of_z_in_s(cosmo.z_start,xinj0,cosmo)['value']
+        return f_inj(X_dm,cosmo)*X_dm.Gamma_inj*(1.+2.*ninj)*np.exp(-X_dm.Gamma_inj*delta_t_stim)
+
+
+
+def dt_stim_dz_of_z_in_s(z,xinj0,cosmo):
+    xinj = xinj0/(1.+z)
+    ninj = n_bb(xinj)
+    return -cosmo.t_H0_in_s()*1./cosmo.E(z)/(1.+z)*(1.+2.*ninj)
+
+def t_stim_of_z_in_s(z,xinj0,cosmo):
+    def integrand(ln1pz,cosmo):
+        zp = np.exp(ln1pz)-1.
+        dzdln1pz= 1.+zp
+        return dt_stim_dz_of_z_in_s(zp,xinj0,cosmo)*dzdln1pz
+    result =  quad(integrand,np.log(1.+1e14),np.log(1.+z), args=cosmo)
+    r_dict = {}
+    r_dict['value']=result[0]
+    r_dict['err'] = result[1]
+    return r_dict
+
+
+def z_of_t_stim(t,xinj0,cosmo):
+    if t<=t_stim_of_z_in_s(cosmo.z_start,xinj0,cosmo)['value']:
+        return cosmo.z_start
+    if t>=t_stim_of_z_in_s(cosmo.z_end,xinj0,cosmo)['value']:
+        return cosmo.z_end
+    def f(ln1pz):
+        z = np.exp(ln1pz)-1.
+        return t_stim_of_z_in_s(z,xinj0,cosmo)['value']-t
+    root = np.exp(optimize.brentq(f, np.log(1.+cosmo.z_start), np.log(1.+cosmo.z_end)))-1.
+    return root
+
+
 
 def get_fdm_from_mu_continuous_injection(mu_limit,cosmo,cosmotherm,dm_particle):
     dm_particle.f_dm = 1.