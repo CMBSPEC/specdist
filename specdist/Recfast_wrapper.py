0bfa397508366886e6e313bd449ed72fd283c1ff
Boris Bolliet <boris.bolliet@gmail.com>
2021-03-17 16:45:16 -0400

    solve PCA with root finding

--- specdist/Recfast_wrapper.py
+++ specdist/Recfast_wrapper.py
@@ -56,11 +56,12 @@ class recfast:
         #######################
 
 
-    def create_tmp_dir_to_store_full_rf_outputs(self):
+    def create_tmp_dir_to_store_full_rf_outputs(self,rm = 'yes'):
         self.tmp_dir_name = self.save_dir_name
         self.path_to_rf_tmp_dir = self.path_to_rf_param_file + self.tmp_dir_name
-        subprocess.call(['rm','-rf',self.path_to_rf_param_file+self.tmp_dir_name])
-        subprocess.call(['mkdir',self.path_to_rf_param_file+self.tmp_dir_name])
+        if rm == 'yes':
+            subprocess.call(['rm','-rf',self.path_to_rf_param_file+self.tmp_dir_name])
+        subprocess.call(['mkdir','-p',self.path_to_rf_param_file+self.tmp_dir_name])
 
 
 
@@ -139,6 +140,114 @@ class recfast:
         r_dict = {**dict_param_values,**dict_rf_results}
         return r_dict
 
+
+    def run_recfast_not_parallel(self,id_gamma,**args):
+        # if id_gamma == 0:
+        self.create_tmp_dir_to_store_full_rf_outputs(rm='no')
+        startTime = datetime.now()
+        id_p = id_gamma
+
+        # pool = multiprocessing.Pool()
+        if args['multi_params'] == 'no':
+            # if type(args['param_values_array'])== float or type(args['param_values_array'])== int:
+            #     array_args = [args['param_values_array']]
+            # else:
+            #     array_args = args['param_values_array']
+            print('no multi params: ', args['param_values_array'])
+            p_val = args['param_values_array'][0]
+            new_ar = np.zeros((id_p+1))
+            new_ar[id_p] = p_val
+            array_args = new_ar
+            # print(array_args)
+            # fn=functools.partial(self.compute_recfast_parallel,param_values_array=array_args,param_name=args['param_name'])
+            # results = pool.map(fn,range(np.size(np.asarray(args['param_values_array']))))
+            results = [self.compute_recfast_parallel(id_p,param_values_array=array_args,param_name=args['param_name'])]
+        elif args['multi_params'] == 'yes':
+            # if type(args['param_values_array'])== float or type(args['param_values_array'])== int:
+            #     array_args = [args['param_values_array']]
+            # else:
+            #     array_args = args['param_values_array']
+
+            # array_args = args['param_values_array']
+            print('with multi params: ', args['param_values_array'])
+            p_val_1 = args['param_values_array'][0][0]
+            p_val_2 = args['param_values_array'][1][0]
+            new_ar_1 = np.zeros((id_p+1))
+            new_ar_2 = np.zeros((id_p+1))
+            new_ar_1[id_p] = p_val_1
+            new_ar_2[id_p] = p_val_2
+            array_args = (new_ar_1,new_ar_2)
+            # array_args = new_ar
+            # fn=functools.partial(self.compute_recfast_parallel_multi_params,param_values_array=array_args,param_name=args['param_name'])
+            #print(len(*param_values_array))
+            # print(np.size(np.asarray(args['param_values_array'][0])))
+            # exit(0)
+            # results = pool.map(fn,range(np.size(np.asarray(args['param_values_array'][0]))))
+            results = [self.compute_recfast_parallel_multi_params(id_p,param_values_array=array_args,param_name=args['param_name'])]
+        # pool.close()
+        #self.clear()
+        #if self.ct_pi_redshift_evolution_mode==0:
+        try:
+            if args['save_recfast_results']=='yes':
+                subprocess.call(['rm','-rf',path_to_recfast_results+'/'+self.save_dir_name])
+                subprocess.call(['mkdir',path_to_recfast_results+'/'+self.save_dir_name])
+                z_rf = []
+                Xe_rf = []
+                # if self.ct_include_pi == 1:
+                #     finj_ct = []
+                # if self.save_Xe == 'yes' and self.ct_evolve_Xe != 0 :
+                #     Xe_values_ct = []
+                #     Xe_redshifts_ct = []
+                for ip in range(len(results)):
+                    z_rf.append(results[ip]['z'])
+                    Xe_rf.append(results[ip]['Xe'])
+                    # if self.ct_include_pi == 1:
+                    #     finj_ct.append(results[ip]['finj'])
+                    # if self.save_Xe == 'yes' and self.ct_evolve_Xe != 0 :
+                    #     Xe_values_ct.append(results[ip]['Xe_values'])
+                    #     Xe_redshifts_ct.append(results[ip]['Xe_redshifts'])
+
+                str_param = 'p'
+                if args['param_name'] == 'photon injection x_dec':
+                    str_param = 'xinj'
+                with open(path_to_recfast_results+'/'+self.save_dir_name + '/' + self.save_dir_name  + '_'+str_param+'_recfast.txt', 'w') as f:
+                    f.write("# arrays of %s for recfast\n"%args['param_name'])
+                    for row in array_args:
+                        np.savetxt(f,[row],fmt="%.3e",delimiter='\t')
+                f.close()
+                # if self.ct_include_pi == 1:
+                #     with open(path_to_ct_spectra_results+'/'+self.save_dir_name + '/' + self.save_dir_name  + '_finj_recfast.txt', 'w') as f:
+                #         f.write("# arrays of finj values for CT spectra\n")
+                #         for row in finj_ct:
+                #             np.savetxt(f,[row],fmt="%.3e",delimiter='\t')
+                #     f.close()
+                with open(path_to_recfast_results+'/'+self.save_dir_name + '/' + self.save_dir_name  + '_Xe_recfast.txt', 'w') as f:
+                    f.write("# arrays of Xe values\n")
+                    for row in Xe_rf:
+                        np.savetxt(f,[row],fmt="%.8e",delimiter='\t')
+                f.close()
+                with open(path_to_recfast_results+'/'+self.save_dir_name + '/' + self.save_dir_name  + '_z_recfast.txt', 'w') as f:
+                    f.write("# arrays of redshift values\n")
+                    for row in z_rf:
+                        np.savetxt(f,[row],fmt="%.3e",delimiter='\t')
+                f.close()
+                # if self.save_Xe == 'yes' and self.ct_evolve_Xe != 0 :
+                #     with open(path_to_ct_spectra_results+'/'+self.save_dir_name + '/spectra_' + self.save_dir_name  + '_Xe_redshifts_ct.txt', 'w') as f:
+                #         f.write("# arrays of redshift values for free electron fraction Xe\n")
+                #         for row in Xe_redshifts_ct:
+                #             np.savetxt(f,[row],fmt="%.3e",delimiter='\t')
+                #     f.close()
+                #     with open(path_to_ct_spectra_results+'/'+self.save_dir_name + '/spectra_' + self.save_dir_name  + '_Xe_values_ct.txt', 'w') as f:
+                #         f.write("# arrays of Xe values for free electron fraction Xe\n")
+                #         for row in Xe_values_ct:
+                #             np.savetxt(f,[row],fmt="%.8e",delimiter='\t')
+                #     f.close()
+        except KeyError:
+            pass
+
+
+
+        return results
 
 
     def run_recfast_parallel(self,**args):
@@ -407,6 +516,159 @@ class recfast:
         return f_dm_pca
 
 
+    # here we don't assume a linear scaling but solve the equation for fdm exactly:
+    def pi_run_pca_constraints_with_recfast_exact(self,xinj_values,gammai_array,id_gamma,fdmi_array,N_iter = 1,*args,**kwargs):
+        PCA_eigen_modes = kwargs['PCA_modes']
+        recfast = kwargs['recfast']
+        f_dm_pca = {}
+        f_dm_pca['curves'] = []
+        f_dm_pca['xinj'] = []
+
+        store_DXe_Xe = kwargs.get('store_DXe_Xe', 'no')
+
+
+        z1 = PCA_eigen_modes.Xe_PCA_EigenModes['E1']['z']
+        E1 = PCA_eigen_modes.Xe_PCA_EigenModes['E1']['values']
+        z2 = PCA_eigen_modes.Xe_PCA_EigenModes['E2']['z']
+        E2 = PCA_eigen_modes.Xe_PCA_EigenModes['E2']['values']
+        z3 = PCA_eigen_modes.Xe_PCA_EigenModes['E3']['z']
+        E3 = PCA_eigen_modes.Xe_PCA_EigenModes['E3']['values']
+
+        f_E1 = interp1d(z1, E1)
+        f_E2 = interp1d(z2, E2)
+        f_E3 = interp1d(z3, E3)
+
+
+
+
+        for id_iter in range(N_iter):
+            print('iter_id = ',id_iter)
+            print('initial fdms (not parallel) = ', fdmi_array)
+            # in fact we do only one xinj value (TBD: remove the loop)
+            for xinj_asked in xinj_values:
+                xdec = xinj_asked
+                curves = {}
+                curves['Gamma_inj'] = gammai_array
+                if store_DXe_Xe == 'yes':
+                    curves['DXe_Xe'] = []
+                    curves['Xe'] = []
+
+                #str_dir = str("%.3e"%xdec)
+
+                self.rf_xinj0 = xdec
+                self.rf_f_dec = fdmi_array[0] # this is the first guess
+
+                args = {}
+
+                p_name_1 = 'Gamma_dec'
+                p_array_1 = gammai_array
+
+                p_name_2 = 'f_dec'
+                p_array_2 = fdmi_array
+
+                args['param_values_array'] = (p_array_1,p_array_2)
+                args['param_name'] = (p_name_1,p_name_2)
+                args['save_recfast_results'] = 'no'
+                args['multi_params'] = 'yes'
+                # args['id_params'] = id_gamma
+                #rf.save_dir_name = 'case_' + '_xdec_' + str_dir
+                R = self.run_recfast_not_parallel(id_gamma,**args)
+
+                # only compute no injection case at first iteration
+                if id_iter == 0:
+                    args['param_name'] =  'Gamma_dec'
+                    args['param_values_array'] = gammai_array
+                    args['save_recfast_results'] = 'no'
+                    args['multi_params'] = 'no'
+                    self.rf_f_dec = 1.e-300
+                    #self.save_dir_name = 'case_no_inj' + '_xdec_' + str_dir
+                    R_no_inj = self.run_recfast_not_parallel(id_gamma,**args)
+
+
+
+                fdm_pca_array = []
+                for k in range(len(R)):
+                    DXe_Xe = (R[k]['Xe']-R_no_inj[k]['Xe'])/R_no_inj[k]['Xe']
+                    Xe = R[k]['Xe']
+                    z_Xe = R[k]['z']
+                    f_DXe_Xe = interp1d(z_Xe,DXe_Xe)
+                    f_Xe = interp1d(z_Xe,Xe)
+
+                    min_z1 = max(np.min(z1),np.min(z_Xe))
+                    max_z1 = min(np.max(z1),np.max(z_Xe))
+
+                    min_z2 = max(np.min(z2),np.min(z_Xe))
+                    max_z2 = min(np.max(z2),np.max(z_Xe))
+
+                    min_z3 = max(np.min(z3),np.min(z_Xe))
+                    max_z3 = min(np.max(z3),np.max(z_Xe))
+
+                    new_z_min = max(min_z1,min_z2,min_z3)
+                    new_z_max = min(max_z1,max_z2,max_z3)
+
+                    new_z_min = 1.01*self.rf_zend
+                    new_z_max = 0.99*4e3
+                    new_z = np.geomspace(new_z_min,new_z_max,5000)
+
+
+                    new_z1 = new_z#np.linspace(min_z1,max_z1,5000)
+                    new_z2 = new_z#np.linspace(min_z2,max_z2,5000)
+                    new_z3 = new_z#np.linspace(min_z3,max_z3,5000)
+
+                    zeta1 = f_DXe_Xe(new_z1)
+                    zeta2 = f_DXe_Xe(new_z2)
+                    zeta3 = f_DXe_Xe(new_z3)
+
+                    integrand_rho1 = zeta1*f_E1(new_z1)
+                    integrand_rho2 = zeta2*f_E2(new_z2)
+                    integrand_rho3 = zeta3*f_E3(new_z3)
+
+
+
+                    rho1 = np.trapz(integrand_rho1, x=new_z1)#/fdmi_array[k]
+                    rho2 = np.trapz(integrand_rho2, x=new_z2)#/fdmi_array[k]
+                    rho3 = np.trapz(integrand_rho3, x=new_z3)#/fdmi_array[k]
+                    #print(rho1,rho2,rho3)
+
+                    sigma1 = 0.117996
+                    sigma2 = 0.194069
+                    sigma3 = 0.351996
+
+                    fdm_pca_lim = 2.*1./np.sqrt(rho1**2/sigma1**2+rho2**2/sigma2**2+rho3**2/sigma3**2)
+                    fdm_pca_array.append(fdm_pca_lim)
+                    if store_DXe_Xe == 'yes':
+                        curves['DXe_Xe'].append([new_z,f_DXe_Xe(new_z)])
+                        curves['Xe'].append([new_z,f_Xe(new_z)])
+
+
+                fdm_pca_array = np.asarray(fdm_pca_array)
+                curves['fdm'] = fdm_pca_array
+
+
+
+
+
+                f_dm_pca['xinj'].append(xdec)
+                f_dm_pca['curves'].append(curves)
+                fdmi_array = curves['fdm']
+        return fdm_pca_array
+
+    def solve_pca_exact(self,id_gamma,gamma_array,xdec,**a_dict):
+        print('solving pca exact')
+        gamma = gamma_array[id_gamma]
+        def f(log10x,gammav,xdecv):
+            x = 10**log10x
+            fdmi_array = [x]
+            N_iter = 1
+            gi_array = [gammav]
+            xdec = [xdecv]
+            return self.pi_run_pca_constraints_with_recfast_exact(xdec,gi_array,id_gamma,fdmi_array,N_iter,**a_dict)[0]-1.
+        try:
+            root = optimize.brentq(f,-20, 0,args = (gamma,xdec))
+            result = 10**root
+        except ValueError:
+            result = np.nan
+        return result
 
 
     def pi_run_pca_constraints_with_recfast_iterative(self,xinj_values,gammai_array,fdmi_array,N_iter = 1,*args,**kwargs):
